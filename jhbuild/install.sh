9b63053a89472c1a6b43af8068572b91566c964c
Stefan Sauer <ensonic@users.sf.net>
2015-01-01 20:26:01 +0100

    jhbuild: add a install.sh script
    This will setup everything and create build/run.sh scripts to keep things
    up-to-date.

---
+++ jhbuild/install.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+#
+# create an empty directory where you want the software to be installed, enter
+# it and run:
+# curl https://raw.githubusercontent.com/Buzztrax/build-tools/master/jhbuild/install.sh | sh
+
+fail=0
+
+which >/dev/null jhbuild
+if [ $? != 0 ]; then
+  echo "E: missing jhbuild tool"
+  echo "please install with your distributions package management"
+  echo "or build from https://git.gnome.org/browse/jhbuild."
+  fail=1
+fi
+
+which >/dev/null curl
+if [ $? != 0 ]; then
+  echo "E: missing curl tool"
+  echo "please install with your distributions package management."
+  fail=1
+fi
+
+if [ $fail == 1 ]; then
+  exit
+fi
+
+REPO=https://raw.githubusercontent.com/Buzztrax/build-tools/master/jhbuild
+curl --remote-name $REPO/buzztrax.modules --remote-name $REPO/buzztrax.jhbuild.rc
+sed -i -e "s#~/buzztrax#$PWD#" buzztrax.jhbuild.rc
+
+cat >build.sh <<SCRIPT
+#!/bin/sh
+jhbuild -f $PWD/buzztrax.jhbuild.rc
+if [ \$? != 0 ]; then
+  echo "E: build failed"
+  exit
+fi
+echo "I: build finished"
+echo "run the application using ./run.sh"
+SCRIPT
+chmod a+x build.sh
+
+cat >run.sh <<SCRIPT
+#!/bin/sh
+jhbuild -f $PWD/buzztrax.jhbuild.rc run $PWD/jhbuild/install/bin/buzztrax-edit
+SCRIPT
+chmod a+x run.sh
+
+./build.sh
+
